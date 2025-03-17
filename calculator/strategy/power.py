65fe5d8656da5c0c5c51b2c6a6a686bbb2981a7f
cmaynard <christine.ri.maynard@gmail.com>
2025-03-16 20:21:21 -0400

    Add unit test for PowerOperation plugin

--- calculator/strategy/power.py
+++ calculator/strategy/power.py
@@ -5,6 +5,6 @@ class PowerOperation(CustomOperation):
     """Raises a number to the power of another."""
     
     def execute(self, a, b):
-        if self.a == 0 and b == 0:
+        if a == 0 and b == 0:
             Validator.validate(a, "Value must be greater than zero")
         return a ** b
