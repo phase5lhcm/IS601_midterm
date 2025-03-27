8ae2551a8211b3dfb1df7a4e5ac4e8f0c87c3e25
cmaynard <christine.ri.maynard@gmail.com>
2025-03-27 03:37:53 -0400

    Refactor implementation of singleton design pattern that maintains single instance of HistoryManager

---
+++ calculator/singleton/tests/test_history_manager.py
@@ -0,0 +1,13 @@
+import pytest
+from faker import Faker
+
+from calculator.singleton.history_manager import HistoryManager
+
+faker = Faker()
+
+
+def test_history_manager():
+    history = HistoryManager()
+    a, b = faker.random_int(min=1, max=100), faker.random_int(min=1, max=100)
+    history.add_record("ADD", a, b, a + b)
+    assert not history.history.empty
